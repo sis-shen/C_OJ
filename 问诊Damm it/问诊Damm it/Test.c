c87e0d65f115846eea59d2746f644b9ec5aea384
sis-shen <adongfa@gmail.com>
2024-01-26 20:52:47 +0800

    155

--- 问诊Damm it/问诊Damm it/Test.c
+++ 问诊Damm it/问诊Damm it/Test.c
@@ -1,25 +1,14 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int base = 7;
+
+int mol = 14;
+
 int main()
 {
-    int n;
-    while (scanf("%d", &n) != EOF)
-    {
-        int a[101] = { 0 };
-        for (int i = 0; i <=  n - 1; i++)
-        {
-            scanf("%d", &a[i]);
-        }
-        
-        if (n % 2 == 0)
-        {
-            printf("No\n");
-        }
-        else {
-            if (a[0] % 2 == 1 && a[n - 1] % 2 == 1)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 .
-                printf("Yes\n");
-            else
-                printf("No\n");
-        }
-    }
-    return 0;
+	char str[200]
+
+	return 0;
 }
