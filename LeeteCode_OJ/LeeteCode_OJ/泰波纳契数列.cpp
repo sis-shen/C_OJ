8e5917ab2cf5d37c91f11ae4f78f6068c17c9fa1
sis-shen <adongfa@gmail.com>
2024-02-03 22:05:46 +0800

    o

---
+++ LeeteCode_OJ/LeeteCode_OJ/泰波纳契数列.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        int a = 0, b = 1, c = 1, d = 2;
+        if (n == 0) return 0;
+        if (n == 1 or n == 2) return 1;
+        for (int i = 3; i <= n; i++)
+        {
+            d = a + b + c;
+            a = b, b = c, c = d;
+        }
+        return d;
+    }
+};
+
+int main()
+{
+	return 0;
+}
--- 比特寒假作业代码/比特寒假作业代码/test.cpp
+++ 比特寒假作业代码/比特寒假作业代码/test.cpp
@@ -2,8 +2,39 @@
 
 #include <stdio.h>
 #include <math.h>
+#include "array.h"
 int main()
 {
-	printf("%lf", pow(10, 1));
+	SeqList SL;
+	SeqList* ps = &SL;
+
+	SLInit(ps);
+	SLPushFront(ps,3);
+	SLPushFront(ps,2);
+	SLPushFront(ps,1);
+
+	SLPrint(ps);
+
+	SLPushBack(ps, 4);
+	SLPushBack(ps, 5);
+	SLPushBack(ps, 6);
+
+	SLPrint(ps);
+
+	SLPopFront(ps);
+
+	SLPrint(ps);
+
+	SLPopBack(ps);
+
+	SLPrint(ps);
+
+	int pos = SLFind(ps, 3);
+	printf("pos = %d\n", pos);
+
+	SLErase(ps,pos);
+	SLPrint(ps);
+
+
 	return 0;
 }
